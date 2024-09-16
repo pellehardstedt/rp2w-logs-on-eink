67b3bd81d6f52fbef088c95984328171425ce50c
Pelle Härdstedt <pelle.hardstedt@gmail.com>
2024-09-16 12:55:31 +0200

    GPIO

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ import sys
 import os
 import subprocess
 import time
+import RPi.GPIO as GPIO
 from waveshare_epd import epd2in13_V2
 from PIL import Image, ImageDraw, ImageFont
 
+# GPIO pin configuration based on the pinout
+RST_PIN = 17
+DC_PIN = 25
+CS_PIN = 8
+BUSY_PIN = 24
+
 def fetch_logs():
     system_logs = subprocess.check_output(['dmesg']).decode('utf-8')
-    
     network_logs = subprocess.check_output(['ifconfig']).decode('utf-8')
-    
     combined_logs = system_logs + "\n\n" + network_logs
     return combined_logs
 
@@ -33,10 +38,22 @@ def display_logs(logs):
         print(e)
 
 def main():
-    while True:
-        logs = fetch_logs()
-        display_logs(logs)
-        time.sleep(2)
+    # GPIO setup
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(RST_PIN, GPIO.OUT)
+    GPIO.setup(DC_PIN, GPIO.OUT)
+    GPIO.setup(CS_PIN, GPIO.OUT)
+    GPIO.setup(BUSY_PIN, GPIO.IN)
+
+    try:
+        while True:
+            logs = fetch_logs()
+            display_logs(logs)
+            time.sleep(4)
+    except KeyboardInterrupt:
+        print("Program interrupted")
+    finally:
+        GPIO.cleanup()
 
 if __name__ == '__main__':
     main()
